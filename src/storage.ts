74ec87131ec9f02b9f054664f40a40526461e0c2
SINE <sinebt@live.de>
2023-03-15 04:17:26 +0100

    Update to work with Deno 1.31.2 * Change behavior of WriteFileStream to make use of Promises to allow for failchecks and waiting for the filesystem before continuing * Change behavior of ReadFileStream to fix issue with reading multiple lines, especially during use of updateOne and added function to extract only json partial from the line, as well as skip lines without json content as empty * Changed test cases slightly * Changed test permission flags to be more inclusive

--- src/storage.ts
+++ src/storage.ts
@@ -1,4 +1,5 @@
-import { EventEmitter } from "std/node/events.ts";
+import { TextLineStream } from "https://deno.land/std@0.179.0/streams/mod.ts";
+import { EventEmitter } from "node:events";
 
 type Doc = Record<string, unknown>;
 
@@ -22,23 +23,68 @@ export async function init(filename: string) {
 
 /** Write file line by line on a stream */
 export class WriteFileStream extends EventEmitter {
-  private file: Deno.FsFile;
+  private file: Promise<Deno.FsFile>;
   private updatedFile: string;
   private readonly encoder = new TextEncoder();
   constructor(private filename: string) {
     super();
+    console.log("WriteFileStream", filename);
     this.updatedFile = `${this.filename}.updated`;
     if (existsFileSync(this.updatedFile)) {
-      Deno.renameSync(this.updatedFile, this.filename);
+      console.log("WriteFileStream", "renaming updated file");
+      this.file = new Promise((resolve) => {
+        try {
+          Deno.open(this.updatedFile, { write: true })
+            .then(async (file) => {
+              await file.close();
+            })
+            .then(() => {
+              Deno.open(this.filename, { write: true })
+                .then(async (file) => {
+                  await file.close();
+                })
+                .then(async () => {
+                  await Deno.renameSync(this.updatedFile, this.filename);
+                  resolve(Deno.openSync(this.updatedFile, {
+                    write: true,
+                    create: true,
+                  }));
+                });
+            });
+        } catch (err) {
+          console.log("WriteFileStream", "error renaming updated file", err);
+          throw new Error("WriteFileStream - error renaming updated file");
+        }
+      });
+    } else {
+      console.log("WriteFileStream", "updated file does not exist yet");
+      this.file = new Promise((resolve) =>
+        resolve(Deno.openSync(this.updatedFile, {
+          write: true,
+          create: true,
+        }))
+      );
     }
-    this.file = Deno.openSync(this.updatedFile, { write: true, create: true });
   }
-  public write(data: Doc) {
-    this.file.writeSync(this.encoder.encode(JSON.stringify(data) + "\n"));
+  public async write(data: Doc) {
+    let thisfile;
+    try {
+      thisfile = await this.file;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+    await thisfile.writeSync(this.encoder.encode(JSON.stringify(data) + "\n"));
     return Promise.resolve(true);
   }
   public async end() {
-    Deno.close(this.file.rid);
+    const thisfile = await this.file;
+    try {
+      await thisfile.close();
+    } catch (err) {
+      console.log("WriteFileStream", "error closing file", err);
+    }
+    //    Deno.close(thisfile.rid);
+
     await Deno.rename(this.updatedFile, this.filename);
     this.emit("close");
   }
@@ -62,14 +108,20 @@ export class ReadFileStream extends EventEmitter {
     this.stream();
   }
   async stream() {
+    console.log("ReadFileStream start");
     const file = await Deno.open(this.filename);
-    for await (const line of file.readable) {
-      if (line.length === 0) {
+    for await (
+      const chunk of file
+        .readable
+        .pipeThrough(new TextDecoderStream())
+        .pipeThrough(new TextLineStream())
+    ) {
+      const json = chunk.match(/\{.*\}/) ?? undefined;
+      if (!json) {
         continue;
       }
-      this.emit("document", JSON.parse(this.decoder.decode(line)));
+      this.emit("document", JSON.parse(json[0]));
     }
-    file.close();
     this.emit("end");
   }
 }
